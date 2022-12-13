a2bc6900091d589f2ad3f5735fceee1ce3cc5e48
Idana Wagner <idanawagner@gmail.com>
2022-12-13 03:25:09 -0300

    Validaciones form

--- src/componentes/Cart/Cart.jsx
+++ src/componentes/Cart/Cart.jsx
@@ -1,4 +1,3 @@
-
 import { Button } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import { useCartContext } from "../../Context/CartContext"
@@ -15,25 +14,24 @@ const Cart = () => {
           <h2>Carrito</h2>
           <ul>
             {cartList.map((product) => <li key={product.id}>
-              <img src={product.photo} className="w-25" />
-              Nombre: {product.name} - Precio {product.price} - cantidad: {product.amount}
-
-              <Button className="m-2" onClick={() => deleteProductCart(product.id)} >Eliminar del carrito</Button>
+              <div className="m-2">
+                <img src={product.photo} className="w-25" />
+                Nombre: {product.name} - Precio $ {product.price} - cantidad: {product.amount}
+                <Button className="m-2" onClick={() => deleteProductCart(product.id)} >Eliminar del carrito</Button>
+              </div>
             </li>
             )}
           </ul>
-          <label htmlFor="">{totalPrice() !== 0 && totalPrice()}</label>
+          <div className="m-3">Total de la compra: {totalPrice() !== 0 && totalPrice()}</div>
           <Button onClick={deleteCart}>Vaciar carrito</Button>
           <Link to='/checkout'><Button>Finalizar compra</Button></Link>
         </>
         :
-
         <>
-          <h2>Tu carrito está vacío </h2>
+          <h2 className="m-5">Tu carrito está vacío </h2>
           <Link to="/">
-            <Button> Ir al inicio </Button>
+            <Button className="mx-5"> Ir al inicio </Button>
           </Link>
-
         </>
       }
     </div>
--- src/componentes/Form/Form.jsx
+++ src/componentes/Form/Form.jsx
@@ -13,49 +13,74 @@ const Form = () => {
 
     //When the user types in the input field, the value of the input field is set to the value of the state.
     const handleOnChange = (evt) => {
-        evt.target.name === ''|| evt.target.email !== evt.target.repeatEmail || evt.target.phone ===''?
-        alert('Faltan datos')
-        :
-
         setDataForm({
             ...dataForm,
-            
             [evt.target.name]: evt.target.value
         })
     }
-
     const handleSubmit = (evt) => {
         evt.preventDefault()
         let order = {}
-        order.buyer = dataForm       // Setting the buyer property of the order object to the dataForm object.
-        order.total = totalPrice()         // A function that is in the context of the cart.
-        order.product = cartList.map(product => ({ id: product.id, name: product.name, price: product.price }))   // A function that is used to map the products that are in the cart.
+        if (dataForm.name == '' || dataForm.email == '' || dataForm.repeatEmail == '' || dataForm.phone == '') {
+            alert('Faltan cargar datos')
+        } else {
+            if (dataForm.email !== dataForm.repeatEmail) {
+                alert('Error de validación: email ingresado incorrecto')
+            } else {
+                order.buyer = dataForm       // Setting the buyer property of the order object to the dataForm object.
+                order.total = totalPrice()         // A function that is in the context of the cart.
+                order.product = cartList.map(product => ({ id: product.id, name: product.name, price: product.price }))   // A function that is used to map the products that are in the cart.
 
-        const db = getFirestore()
-        const queryCollection = collection(db, 'orders')
-        addDoc(queryCollection, order)
-            .then(resp =>setIdOrder(resp.id))   // Setting the state of the idOrder.
-            
-            .finally(() => { 
-                setDataForm( form )   // Resetting the form.
-                deleteProductCart()   // A function that is in the context of the cart. It is used to delete the products from the cart.
-            
-        })
+                const db = getFirestore()
+                const queryCollection = collection(db, 'orders')
+                addDoc(queryCollection, order)
+                    .then(resp => setIdOrder(resp.id))   // Setting the state of the idOrder.
+                    .finally(() => {
+                        setDataForm(form)   // Resetting the form.
+                        deleteProductCart()   // A function that is in the context of the cart. It is used to delete the products from the cart.
+                    })
+            }
+        }
     }
     return (
         <>
             {idOrder ?
-            <>
-            <span>Muchas gracias por su compra, su código de orden es: {idOrder}</span>
-            </>
-            : 
-                <form onSubmit={handleSubmit}>
-                    <input type="text" name='name' placeholder='Ingrese nombre' value={dataForm.name} onChange={handleOnChange} />
-                    <input type="text" name='email' placeholder='Ingrese email' value={dataForm.email} onChange={handleOnChange} />
-                    <input type="text" name='repeatEmail' placeholder='Repetir email' onChange={handleOnChange} />
-                    <input type="text" name='phone' placeholder='Ingrese teléfono' value={dataForm.phone} onChange={handleOnChange} />
+                <div className='m-5'>
 
-                    <Button onClick={handleSubmit.bind()}>Enviar pedido</Button>
+                    <span>Muchas gracias por su compra</span>
+                    <div>Su código de orden es: {idOrder}</div>
+                </div>
+                :
+                <form className='m-1' onSubmit={handleSubmit}>
+                    <div className="row m-4">
+                        <div className='col-md-12'>
+                            <label className="col-sm-3 col-form-label">Nombre: </label>
+                            <div className="col-sm-10">
+
+                                <input type="text" className="form-control" name='name' placeholder='Ingrese nombre' value={dataForm.name} onChange={handleOnChange} />
+                            </div>
+                        </div>
+                        <div className="col-md-12">
+                            <label className="col-sm-3 col-form-label">Telefono:</label>
+                            <div className="col-sm-10">
+
+                                <input type="text" className="form-control" name='phone' placeholder='Ingrese teléfono' value={dataForm.phone} onChange={handleOnChange} />
+                            </div>
+                        </div>
+                        <div className="col-md-12">
+                            <label for="inputEmail3" className="col-sm-3 col-form-label"> Email:</label>
+                            <div className="col-sm-10">
+                                <input type="email" name='email' className="form-control" placeholder='Ingrese email' value={dataForm.email} onChange={handleOnChange} />
+                            </div>
+                        </div>
+                        <div className="col-md-12">
+                            <label for="inputEmail3" className="col-sm-3 col-form-label">Repetir Email:</label>
+                            <div className="col-sm-10">
+                                <input type="email" className="form-control" name='repeatEmail' placeholder='Repetir email' onChange={handleOnChange} />
+                            </div>
+                        </div>
+                    </div>
+                    <Button className='ms-5' onClick={handleSubmit.bind()}>Enviar pedido</Button>
                 </form>
             }
         </>
